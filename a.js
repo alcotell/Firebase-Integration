6b1a23e828fe01d1a125fb4a91408752665451e1
akshitbrightlifecare <akshit.sawhney@brightlifecare.com>
2016-10-05 10:04:20 +0530

    Initial commit
    Initial commit

---
+++ a.js
@@ -0,0 +1,37 @@
+{
+    "country": {
+        "india": {
+            "category": {
+                "beer": {
+                    "brand": {
+                        "Kingfisher": {
+                            "ABV": "4%"
+                        },
+                        "corona": {
+                            "ABV": "6%"
+                        }
+                    },
+                    "property": {
+                        "discription": "Sindhi Rocks"
+                    }
+                },
+                "whiskey": {
+                    "brand": {
+                        "Kingfisher": {
+                            "ABV": "4%"
+                        },
+                        "corona": {
+                            "ABV": "6%"
+                        }
+                    },
+                    "property": {
+                        "discription": "Sindhi Rocks"
+                    }
+                }
+            }
+        }
+    },
+    "glassware": {
+
+    }
+}
